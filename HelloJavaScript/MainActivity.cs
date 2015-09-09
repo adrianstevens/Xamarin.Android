1e9d5e63610c9314d10cb8c488acfa7cf6fe9d5d
Adrian Stevens <adrian.stevens@gmail.com>
2015-09-09 16:58:04 -0700

    Added a simple example of using JavaScript within an Android WebView

---
+++ HelloJavaScript/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+using System.IO;
+
+namespace HelloJavaScript
+{
+	[Activity (Label = "HelloJavaScript", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			WebView wv = FindViewById<WebView> (Resource.Id.webView);
+			wv.Settings.JavaScriptEnabled = true;
+
+			Stream input = Assets.Open ("hello.html");
+
+			StreamReader sr = new StreamReader (input);
+
+			var s = sr.ReadToEnd ();
+
+			wv.LoadDataWithBaseURL ("file:///android_asset/", s , "text/html", "UTF-8", null);
+
+		
+		}
+	}
+}
+
+
